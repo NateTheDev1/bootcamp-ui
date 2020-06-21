ab3886dd0da6332d907fd0819f0f990cb6b7eb8e
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-20 23:07:55 -0400

    Initial

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  const styles = {
+    title: {
+      color: 'blue'
+    }
+  }
+  return <div style={styles.title}>Example Component: {text}</div>
 }
